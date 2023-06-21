33bee6cc1451baa1390167f1a058d5a64d16c8ca
student1060632fpt <haveaniceday6174@gmail.com>
2023-06-21 19:36:32 +0700

    cải tiến mới

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
+import random
+import time
+
 from Adafruit_IO import MQTTClient
 
-AIO_FEED_ID = "bbc -led"
+AIO_FEED_ID = "bbc-led"
 AIO_USERNAME = "haveaniceday6174"
-AIO_KEY = "aio_ztim191bop1o2wQtUP6aXnT7LmKN"
+AIO_KEY = "aio_JTYj62T1XnRRKMjLovE1U06S7D8F"
 
 def connected(client):
     print("Ket noi thanh cong")
@@ -19,7 +22,7 @@ def disconnected(client):
 def message(client, feed_id, payload):
     print("Nhan du lieu: " + payload)
 
-client = MQTTClient(AIO_USERNAME, AIO_KEY)
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
 client.on_connect = connected
 client.on_disconnect = disconnected
 client.on_message = message
@@ -27,7 +30,5 @@ client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
 
-while True:
+while True :
     pass
-
-print("in success")
