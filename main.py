4db4d5360b59ef90aee80deca8524d86b625a35b
student1060632fpt <haveaniceday6174@gmail.com>
2023-06-21 19:22:23 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+from Adafruit_IO import MQTTClient
+
+AIO_FEED_ID = "bbc -led"
+AIO_USERNAME = "haveaniceday6174"
+AIO_KEY = "aio_ztim191bop1o2wQtUP6aXnT7LmKN"
+
+def connected(client):
+    print("Ket noi thanh cong")
+    client.subscribe(AIO_FEED_ID)
+
+def subscribe(client, userdata, mid, granted_qos):
+    print("Subcribe thanh cong")
+
+def disconnected(client):
+    print("Ngat ket noi")
+    sys.exit(1)
+
+def message(client, feed_id, payload):
+    print("Nhan du lieu: " + payload)
+
+client = MQTTClient(AIO_USERNAME, AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+
+while True:
+    pass
+
+print("in success")
